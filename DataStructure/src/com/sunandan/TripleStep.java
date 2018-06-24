fd0ecbb47916c9ab91dd9b2ac5c66631603add17
sunandan <sunandan.riders@gmail.com>
2018-06-24 12:28:58 +0530

    modifying Triple class

--- DataStructure/src/com/sunandan/TripleStep.java
+++ DataStructure/src/com/sunandan/TripleStep.java
@@ -3,19 +3,27 @@ package com.sunandan;
 public class TripleStep {
 
 	
-	static int step[] = {1,2,4} ;
+	static int[] step ;
+		
 	public static void main(String[] args) {
 		int n=5;
+		step = new int[n];
+		step[0] = 1;
+		step[1] = 2;
+		step[2] = 4;
 		int possibleWays = countStep(n);
 		System.out.println(possibleWays);
 	}
 	private static int countStep(int n) {
+		if(step[n-1]!=0)
+			return step[n-1];
 		if(n<1)
 			return 0;
 		if(n<=3)
 			return step[n-1];
 		else
-			return countStep(n-1)+countStep(n-2)+countStep(n-3);
+			step[n-1]=countStep(n-1)+countStep(n-2)+countStep(n-3);
+			return step[n-1];
 			
 	}
 
