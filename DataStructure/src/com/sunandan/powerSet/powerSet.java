a7fe5d282cc52ef6c568d0de49f0b8453ad42a50
sunandan <sunandan.riders@gmail.com>
2018-06-27 00:23:37 +0530

    Adding Question power set

---
+++ DataStructure/src/com/sunandan/powerSet/powerSet.java
@@ -0,0 +1,23 @@
+package com.sunandan.powerSet;
+
+public class powerSet
+{
+    static void printSubsets(char set[])
+    {
+        int n = set.length;
+        for (int i = 0; i < (1<<n); i++)
+        {
+            System.out.print("{ ");
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) > 0)
+                    System.out.print(set[j]+ " ");
+            }
+            System.out.println("}");
+        }
+    }
+    public static void main(String[] args)
+    {
+        char set[] = {'a', 'b', 'c' , 'd'};
+        printSubsets(set);
+    }
+}
