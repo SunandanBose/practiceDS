b8f72c9b6fa29155298c0520d171a8a85fb664fd
sunandan <sunandan.riders@gmail.com>
2018-06-27 23:50:20 +0530

    Adding recursive Multiply

---
+++ DataStructure/src/com/sunandan/recursiveMultiply/RecursiveMultiply.java
@@ -0,0 +1,21 @@
+package com.sunandan.recursiveMultiply;
+
+public class RecursiveMultiply {
+
+	public static void main(String[] args) {
+		int n1 = 24;
+		int n2 = 46;
+		int product = recurMultiply(n1, n2);
+		System.out.println(product);
+	}
+
+	private static int recurMultiply(int x, int y) {
+		if(y==0)
+			return 0;
+		else if(y<0)
+			return (-x+recurMultiply(x,y+1));
+		else
+			return (x+recurMultiply(x,y-1));
+	}
+
+}
