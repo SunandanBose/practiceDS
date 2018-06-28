4e896c469016b1d219ae77d2c064d0cba35ff561
Raj <rajkum@thoughtworks.com>
2018-06-28 22:26:32 +0530

    Commiting test

--- DataStructure/src/com/raj/powerset/powerset.py
+++ DataStructure/src/com/raj/powerset/powerset.py
@@ -5,7 +5,7 @@ def powerset(inputSet):
 	for x in inputSet:
 		newSet = []
 		for element in resultSet:
-			value = x+element
+			value = x + element
 			newSet.append(value)
 		resultSet.extend(newSet)
 
