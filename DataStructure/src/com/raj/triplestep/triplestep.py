1d9fbbfd29183c0849727dbb21200bb0ca354f63
Raj <rajkum@thoughtworks.com>
2018-07-03 07:24:05 +0530

    Adding comments and sample

--- DataStructure/src/com/raj/triplestep/triplestep.py
+++ DataStructure/src/com/raj/triplestep/triplestep.py
@@ -1,12 +1,16 @@
 import sys
 
-countOfTriples={0: 1, 1: 1, 2: 2, 3: 4}
+countOfSteps={0: 1, 1: 1, 2: 2, 3: 4}
 def triple(n):
-	if (-1 != countOfTriples.get(n, -1)):
-		return countOfTriples[n]
-	countOfN = triple(n-1) + triple(n-2) + triple(n-3)
-	countOfTriples[n] = countOfN
-	return countOfN
+	# Memoization check if already present in dictionary
+	if (-1 != countOfSteps.get(n, -1)):
+		return countOfSteps[n]
+
+	# Number of steps is equal to sum of steps a child can 
+	# cover the previous 3 steps.
+	numberOfSteps = triple(n-1) + triple(n-2) + triple(n-3)
+	countOfSteps[n] = numberOfSteps
+	return numberOfSteps
 
 print triple(int(sys.argv[1]))
 
