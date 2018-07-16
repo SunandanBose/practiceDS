c0b81944b208239db915b8b4b65a1670f37c7291
Raj <rajkum@thoughtworks.com>
2018-07-16 08:04:26 +0530

    Handling multiplication by 0
    Signed-off-by: Raj <rajkum@thoughtworks.com>

--- DataStructure/src/com/raj/recursive_multiply/multiply.py
+++ DataStructure/src/com/raj/recursive_multiply/multiply.py
@@ -1,6 +1,8 @@
 import sys
 def multiply(num1, num2):
 	print("{ num1 , "+ str(num1) +" num2 " + str(num2) + " }")
+	if num1 == 0 or num2 == 0:
+		return 0
 	if num1 == 1:
 		print("I'm exiting" + str(num2))
 		return num2
