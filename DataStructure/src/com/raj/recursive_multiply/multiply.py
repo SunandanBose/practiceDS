b618e29e0b8f4e6f20abdd11e97bb1abf3030cf6
Raj <rajkum@thoughtworks.com>
2018-07-16 08:02:44 +0530

    Reducing run time by half.

--- DataStructure/src/com/raj/recursive_multiply/multiply.py
+++ DataStructure/src/com/raj/recursive_multiply/multiply.py
@@ -1,10 +1,12 @@
 import sys
 def multiply(num1, num2):
 	print("{ num1 , "+ str(num1) +" num2 " + str(num2) + " }")
-	if(num1 == 1):
+	if num1 == 1:
 		print("I'm exiting" + str(num2))
 		return num2
 	else:
+		if num1 > 2:
+			return (multiply(num1 - 2, num2)) + num2 + num2
 		return (multiply(num1 - 1, num2)) + num2
 
 print (multiply(int(sys.argv[1]), int(sys.argv[2])))
