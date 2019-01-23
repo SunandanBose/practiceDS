31fdd53648273e3fcf13fe4d830aaaf3065be14b
SunandanBose <sunandan.riders@gmail.com>
2019-01-23 13:50:25 +0530

    Refactored the code for better performance

--- DataStructure/src/com/sunandan/permutation/Permutation.java
+++ DataStructure/src/com/sunandan/permutation/Permutation.java
@@ -1,50 +1,29 @@
 package com.sunandan.permutation;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.stream.Collectors;
 
-public class Permutation
-{
-	static List<String> permutationString = new ArrayList<>();
-	
-    public static void main(String[] args)
-    {
-    	
-    	String str = "ABCDE";
-        new Permutation().permute(str, 0, str.length()-1);
-        System.out.println(permutationString.size());
-        permutationString.forEach(System.out::println);
-        
-    }
-    private void permute(String str, int initial, int last)
-    {
-        if(initial == 0 && last ==0) { 
-        	permutationString.add(str);
-        	permute(str,initial+1,str.length()-1);
-        }
-    	if(initial==last)
-        	return;
-        permute(str,initial,last-1);
-        if(str.charAt(initial) != str.charAt(last)){
-            str = swap(str,initial,last);
-            if(!permutationString.contains(str))
-                permutationString.add(str);
-        }
-        permute(str,initial+1,str.length()-1);
-    }
+public class Permutation {
 
-    public String swap(String a, int i, int j)
-    {
-    	if(i==j)
-    		return a;
-        char temp;
-        char[] charArray = a.toCharArray();
-        temp = charArray[i] ;
-        charArray[i] = charArray[j];
-        charArray[j] = temp;
-        return String.valueOf(charArray);
-    }
- 
+	static Queue<String> queueList = new LinkedList<String>();
+	public static void main(String[] args) {
+		String str = "ABC";
+		queueList.add(Character.toString(str.charAt(0)));
+		permute(str);
+		queueList.stream().collect(Collectors.toSet()).forEach(System.out::println);
+	}
+	private static void permute(String str) {
+		while(true){
+			if(queueList.peek().length() ==  str.length())
+				return;
+			getPermutationsOfString(str,queueList.peek(),0);
+			queueList.remove();
+		}
+	}
+	private static void getPermutationsOfString(String str, String newstr, int index) {
+		if(index > newstr.length()) return;
+		queueList.add(new StringBuffer().append(newstr).insert(index, str.charAt(newstr.length())).toString());
+		getPermutationsOfString(str,newstr,index += 1);
+	}
 }
