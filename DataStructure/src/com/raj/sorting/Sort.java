8c5e987ea96e5a1ebf6f0ef37fbe581a0efe0d0d
rkumar <rkumar@agoda.com>
2019-10-18 00:31:01 +0700

    WIP

--- DataStructure/src/com/raj/sorting/Sort.java
+++ DataStructure/src/com/raj/sorting/Sort.java
@@ -28,6 +28,19 @@ class Sort {
     }
 
 
+
+    private List<Integer> merge(List<Integer> list_1, List<Integer> list_2) {
+		int c1 = 0, c2 = 0;
+	    List<Integer> mergedList = new ArrayList<Integer>();
+	    while(c1 < list_1.size() || c2 < list_2.size()) {
+	    	if(c2 < list_2.size())
+	        	mergedList.add((Integer) list_2.get(c2++));
+	    	if(c1 < list_1.size())
+	        	mergedList.add((Integer) list_1.get(c1++));
+	    }
+	    return mergedList;
+	}
+
     private void swap(List<Integer> list, int i,  int j){
         int temp;
         temp = list.get(i);
