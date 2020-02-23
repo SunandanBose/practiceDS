17c1effd45c1a8ee1aec2ecb62c961f51ff51152
Sunandan Bose <sunandan.riders@gmail.com>
2020-02-23 19:31:20 +0530

    Found Lowest Common Factor of a Binary Tree

---
+++ DataStructure/src/com/sunandan/tree/LCABinaryTree.java
@@ -0,0 +1,71 @@
+//Lowest Common Ancestor Binary tree
+import java.util.*;
+
+class LCABinaryTree<T>{
+
+
+    public static void main(String[] args) {
+        LCABinaryTree<Integer> lca= new LCABinaryTree<>();
+        int[] arr = {1,2,3,4,5,6,7};
+        Node<Integer> rootTreeNode=lca.createBinaryTreeWithSortedArray(arr,0,arr.length-1);
+        Integer lcaValue = lca.findLCA(rootTreeNode,7,1);
+        if(lcaValue != null)
+            System.out.println("Lowest Common Factor is : "+lcaValue);
+        else
+            System.out.println("No Common Ancestor found.\nThe value given may not be present.");
+    }
+
+    private T findLCA(Node<T> rootTreeNode, Object value1, Object value2){
+        int index=0;
+        List<T> commonNodesList = new ArrayList<>();
+        List<T> path1List = getPath(rootTreeNode,value1);
+        List<T> path2List = getPath(rootTreeNode,value2);
+        while(path1List.size()>index && path2List.size()>index){
+            if(path1List.get(index).equals(path2List.get(index))){
+                commonNodesList.add(path1List.get(index));
+            }
+            index++;
+        }
+        if(commonNodesList.size() > 0 )
+            return  commonNodesList.get(commonNodesList.size()-1) ;
+        return null;
+    }
+
+    private   List<T> getPath(Node<T> root, Object value){
+        List<T> path = new ArrayList<>();
+        path.add(root.data);
+        if(root.data.equals(value)){
+            return Collections.singletonList(root.data);
+        }
+        if(root.left!=null){
+            path.addAll(getPath(root.left, value));
+            if(path.get(path.size()-1)==value)
+                return path;
+        }
+        if(root.right!=null){
+            path.addAll(getPath(root.right, value));
+            if(path.get(path.size()-1)==value)
+                return path;
+        }
+        return Collections.emptyList();
+    }
+
+    private Node<Integer> createBinaryTreeWithSortedArray(int[] arr,int first_pos,int last_pos){
+        if(first_pos>last_pos)
+                return null;
+        int mid = (first_pos + last_pos)/2;
+        Node<Integer> node = new Node<Integer>(arr[mid]);
+        node.left = createBinaryTreeWithSortedArray(arr, first_pos, mid-1);
+        node.right = createBinaryTreeWithSortedArray(arr, mid+1, last_pos);
+        return node;
+    }
+}
+
+ class Node<T>{
+    T data;
+    Node<T> right;
+    Node<T> left;
+    Node(T data){
+        this.data = data;
+    }
+}
