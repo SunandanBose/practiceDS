96718e4d0f0ae2af58f3c8cfa727812c608b8704
Sunandan Bose <sunandan.riders@gmail.com>
2020-04-21 21:00:17 +0530

    Refactor CoinChangeProblem

--- DataStructure/src/main/java/com/sunandan/permutation/CoinChangeProblem.java
+++ DataStructure/src/main/java/com/sunandan/permutation/CoinChangeProblem.java
@@ -7,15 +7,8 @@ import java.util.*;
  class CoinChangeProblem{
 
 
-
- 	public static void main(String args[]){
-
- 		List<Integer> coinDeminations = Arrays.asList(1,2,3);
- 		getDenomination(new ArrayList<>(),coinDeminations, 4);
-
- 	}
   
- 	private static void getDenomination(List<Integer> resultSet, List<Integer> coinDeminations, int finalCoinAmount){
+ 	public void getDenomination(List<Integer> resultSet, List<Integer> coinDeminations, int finalCoinAmount){
  		int sum = resultSet.stream().reduce(0, (a, b) -> a + b);
  		if( sum == finalCoinAmount){
  			resultSet.forEach(System.out::print);
