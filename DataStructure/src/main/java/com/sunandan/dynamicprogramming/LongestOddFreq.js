3ee2a24e1400b61e079dec146cc1130759cbdc30
Sunandan Bose <sunandan.riders@gmail.com>
2020-05-10 19:25:14 +0530

    Refactor LongestOddFreq

--- DataStructure/src/main/java/com/sunandan/dynamicprogramming/LongestOddFreq.js
+++ DataStructure/src/main/java/com/sunandan/dynamicprogramming/LongestOddFreq.js
@@ -2,28 +2,36 @@
 // Find the Maximum Number of Times an odd Number is continously repeated in the array
 
 function LongestOddFreq(input){
-    let freq = {};
+    let maxCounter = 0;
+    let current = -1;
+    let currentCounter = 0;
     for(let i = 0; i < input.length; i++){
-        if(input[i]%2===0)  continue;
-        for(let j=i; j < input.length; j++){
-            if(input[i] !== input[j] || j == input.length-1){
-                if(freq[input[i]]){
-                    if(freq[input[i]] < j-i){
-                        freq[input[i]] = j-i;
-                        j == input.length-1 ? i = j : i = j-1;  
-                        break;
-                    }
-                }
-                freq[input[i]] = j-i;
-                j == input.length-1 ? i = j : i = j-1;
-                break;
-            }     
+        if(input[i] % 2 === 0) {
+            if (currentCounter > maxCounter) {
+                maxCounter = currentCounter;
+            }
+            current = input[i + 1]
+            currentCounter = 0;
+            continue;
         }
+        if(input[i] !== current) {
+            if (currentCounter > maxCounter) {
+                maxCounter = currentCounter;
+            }
+            current = input[i]
+            currentCounter = 1;
+            continue;
+        }
+        currentCounter++;
     }
-    return Object.values(freq).sort().reverse()[0];
+    return maxCounter || currentCounter;
 }
 
-console.log(LongestOddFreq(['1', '1', '1', '1', '2', '2', '2', '2', '2', '1', '1', '1', '1', '1', '1','3','3','3','3']));
+ console.log(LongestOddFreq(['1', '1', '1', '1']));
+ console.log(LongestOddFreq(['2', '2', '2', '2']));
+ console.log(LongestOddFreq(['1', '2', '2', '2']));
+ console.log(LongestOddFreq(['2', '2', '1', '2']));
+ console.log(LongestOddFreq(['1', '1', '1', '1', '2', '2', '2', '2', '2', '1', '1', '1', '1', '1', '1','3','3','3','3']));
 
 
 
