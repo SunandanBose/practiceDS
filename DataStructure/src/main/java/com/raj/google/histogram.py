cfc0992da5f666cfbe8b85d4c88ea9ece25959e6
rkumar <rkumar@agoda.com>
2020-05-23 10:35:45 +0700

    Histogram test in python

--- DataStructure/src/main/java/com/raj/google/histogram.py
+++ DataStructure/src/main/java/com/raj/google/histogram.py
@@ -1,17 +1,14 @@
-# Find max area in histogram
-
-# Representation: 
-
-# [1,3,4,2,5]
+import logging
+logger = logging.getLogger(__name__)
 
 class Histogram:
-    def __init__(self):
-        self.values = [1,3,4,2,5]
+    def __init__(self, histogram):
+        self.values = histogram
     
     def display(self):
         max_value = max(self.values)
         for value in self.values:
-            print (value * "#")
+            logger.info (value * "#")
 
     def calculateMaxAreaON(self):
         stack = []
@@ -20,19 +17,19 @@ class Histogram:
             # Insert in empty stack or insert if current value is greater than previous value
             # import pdb; pdb.set_trace()
             if not stack or (self.values[stack[len(stack) - 1]] <= value):
-                print('insertion in empty or upwards stack')
+                logger.info('insertion in empty or upwards stack')
                 stack.append(index)
-                print(stack)
+                logger.info(stack)
                 continue
             top = len(stack) - 1
             # pop until stack is empty or stack top is less than value
-            
-            while stack and stack[top] <= value:
+            # import pdb; pdb.set_trace()
+            while stack and self.values[stack[top]] > value:
                 # Pop the top most index of stack and get its value
                 currentPoppedValue = self.values[stack.pop()]
                 top = len(stack) - 1
                 if not stack:
-                    print('empty stack processing')
+                    logger.info('empty stack processing')
                     # This is true because you cannot have a value lower than the current value so far with all elements
                     # Examples: 2,3,4,1,3
                     # So for 1 is obviously inclusive of all.
@@ -40,7 +37,7 @@ class Histogram:
                     currentValue = currentPoppedValue * index
                     maxValue = maxValue if maxValue > currentValue else currentValue
                 else: 
-                    print('processing stack with values')
+                    logger.info('processing stack with values')
                     # Examples: 2,3,4,1,3
                     # We do index - 1 because when we popped 4
                     # The first iteration we want all values from index of 4 which is 3 upto index of 2 excluding 2
@@ -51,18 +48,19 @@ class Histogram:
                     maxValue = maxValue if maxValue > currentValue else currentValue
             # push value either on empty stack or because value is greater than or equal to stack top
             stack.append(index)
-            print(stack)
+            logger.info(stack)
+        index = index + 1
         while stack:
-            print('overflow values for stack')
+            logger.info('overflow values for stack')
             # Pop the top most index of stack and get its value
             currentPoppedValue = self.values[stack.pop()]
+            top = len(stack) - 1
             if not stack:
                 currentValue = currentPoppedValue * index
+
                 maxValue = maxValue if maxValue > currentValue else currentValue
             else: 
                 currentValue = currentPoppedValue * (index - 1 - stack[top])
+
                 maxValue = maxValue if maxValue > currentValue else currentValue
         return maxValue
-
-Histogram().display()
-print(Histogram().calculateMaxAreaON())
