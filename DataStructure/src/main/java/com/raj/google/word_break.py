c260430f1a80b5d5096905c6b96e389fa521e2e3
rkumar <rkumar@agoda.com>
2020-06-02 09:09:46 +0700

    Trie find

--- DataStructure/src/main/java/com/raj/google/word_break.py
+++ DataStructure/src/main/java/com/raj/google/word_break.py
@@ -37,13 +37,28 @@ class Trie:
             if char in currentDict:
                 currentNode = currentDict[char]
             else: 
-                print (currentDict)
                 currentDict[char] = Node()
                 currentNode = currentDict[char]
             wordToInsert = wordToInsert[1:]
         currentNode.isWordEnding = True
 
-            
-Trie().add('ram')
+    def isWordPresent(self, word):
+        wordToFind = word
+        currentNode = self.node
+        while len(wordToFind) > 0:
+            currentDict = currentNode.dictionary
+            char = wordToFind[:1]
+            if char in currentDict:
+                currentNode = currentDict[char]
+                wordToFind = wordToFind[1:]
+            else: return False
+        
+        return currentNode.isWordEnding
+        
+trie = Trie()            
+trie.add('ram')
+print(trie.isWordPresent('ram'))
+print(trie.isWordPresent('ra'))
+print(trie.isWordPresent('rama'))
                 
             
