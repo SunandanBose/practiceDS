fde10fe223f5e869b8c78a13019b18be4875e8ef
rkumar <rkumar@agoda.com>
2020-06-10 23:09:51 +0700

    Storing the results

--- python/dynamic_programming/coin_change.py
+++ python/dynamic_programming/coin_change.py
@@ -1,14 +1,11 @@
-coins = []
-amount = 25
-
 class CoinChange:
     def __init__(self):
-        None
+        self._map = {}
 
     def get_coins(self, coins, amount):
         return []
 
-    def count(self, coins, amount):
+    def count(self, amount, coins):
         return self.__count(coins, amount, 0)
 
     def __count(self, coins, amount, index):
@@ -18,4 +15,9 @@ class CoinChange:
             return 1
         if (index == len(coins) and amount is not 0):
             return 0
-        return self.__count(coins, amount - coins[index], index) + self.__count(coins, amount, index + 1)
+        key = str(amount) + '@' + str(index)
+        if key in self._map:
+            return self._map[key]
+        count = self.__count(coins, amount - coins[index], index) + self.__count(coins, amount, index + 1)
+        self._map[key] = count
+        return count
