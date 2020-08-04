2b55820575b2fd946b711fe724a08303875c0755
Sunandan Bose <sunandan.riders@gmail.com>
2020-08-04 21:24:06 +0530

    Create Snake and Ladder Game

---
+++ DataStructure/src/main/java/com/sunandan/snakeAndLadder/Game.java
@@ -0,0 +1,16 @@
+package com.sunandan.snakeAndLadder;
+
+import java.util.LinkedList;
+
+public class Game {
+
+    LinkedList<Player> players = new LinkedList<Player>();
+
+    Board board = new Board();
+
+    public static void main(String[] args) {
+        System.out.println("Game has Started");
+    }
+
+
+}
---
+++ DataStructure/src/main/java/com/sunandan/snakeAndLadder/Player.java
@@ -0,0 +1,4 @@
+package com.sunandan.snakeAndLadder;
+
+public class Player {
+}
