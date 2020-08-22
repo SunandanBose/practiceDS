f6bdb51895c1d53aee867c7eba738d1da3243f38
Sunandan Bose <sunandan.riders@gmail.com>
2020-08-22 23:13:47 +0530

    Test

--- updateReadMe.sh
+++ updateReadMe.sh
@@ -2,3 +2,8 @@ lastLine=$(tail -n 1 README.md)
 echo $lastLine
 serialNumber=$(echo "$lastLine" | cut -d "|" -f1)
 echo $((serialNumber+1))
+newSerialNumber=$((serialNumber+1))
+#$(git diff-tree --no-commit-id --name-status -r "$local_sha")
+read filename
+echo Your filename id $filename
+echo $filename >> README.md
