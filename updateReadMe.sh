01e025c32bfc37791b6fdf70b360a27173f3b3c0
Sunandan Bose <sunandan.riders@gmail.com>
2020-08-23 00:40:42 +0530

    Update Readme Script

--- updateReadMe.sh
+++ updateReadMe.sh
@@ -3,7 +3,21 @@ echo $lastLine
 serialNumber=$(echo "$lastLine" | cut -d "|" -f1)
 echo $((serialNumber+1))
 newSerialNumber=$((serialNumber+1))
-#$(git diff-tree --no-commit-id --name-status -r "$local_sha")
-read filename
-echo Your filename id $filename
-echo $filename >> README.md
+commit=$(git diff-tree --diff-filter=A --no-commit-id --name-status -r "$local_sha" | tr 'A ' ' ')
+echo $commit 
+IFS=' ' #setting space as delimiter  
+read -ra ADDR <<<"$commit" #reading commit str as an array as tokens separated by IFS  
+  
+for i in "${ADDR[@]}"; #accessing each element of array  
+do  
+	echo Do you want of continue\?\(y\/n\)
+	read ans
+	if [[ "$ans" == "Y" || "$ans" == "y" ]]; then
+    	echo Enter the code link for file $i
+		read filename
+		echo filename you have entered is $filename
+		echo $newSerialNumber \| Distribute Candies to People \|[Explanation]\($filename\)\| [Code]\($i\)\|| >> README.md
+	fi
+	
+	  
+done
