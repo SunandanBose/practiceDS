537a40f3c7a3b3ece10d604b959a4b47de651e3c
Sunandan Bose <sunandan.riders@gmail.com>
2020-08-31 23:54:43 +0530

    modify coin change problem

--- DataStructure/src/main/java/com/sunandan/permutation/CoinChangeProblem.java
+++ DataStructure/src/main/java/com/sunandan/permutation/CoinChangeProblem.java
@@ -6,7 +6,6 @@ import java.util.*;
 class CoinChangeProblem {
 	public List<List<Integer>> getDenomination(List<Integer> coinDenominations, int finalCoinAmount) {
 		List<List<Integer>> resultList = new ArrayList<>();
-		HashMap<Integer, List<Integer>> found = new HashMap<>();
 		for (Integer coinValue : coinDenominations) {
 		    List<Integer> combinations = new ArrayList<>();
 		    if (coinValue > finalCoinAmount){
@@ -17,15 +16,10 @@ class CoinChangeProblem {
                 return resultList;
             }
 		    combinations.add(coinValue);
-		    if(found.get(coinValue) != null){
-                List<Integer> integers = found.get(coinValue);
-                return Collections.singletonList(integers);
-            }
             List<List<Integer>> intermediateCombinations = getDenomination(coinDenominations, finalCoinAmount - coinValue);
 		    for (List<Integer> intermediate : intermediateCombinations){
 		        intermediate.addAll(combinations);
             }
-		    found.put(coinValue, combinations);
 		    resultList.addAll(intermediateCombinations);
 
 		}
