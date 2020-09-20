95b2d8ecd8f050fc2c53b0c7e97538861409f86d
Sunandan Bose <sunandan.riders@gmail.com>
2020-09-20 17:44:53 +0530

    Formatted Indentation of Repeated String

--- DataStructure/src/main/java/com/sunandan/permutation/RepeatedString.java
+++ DataStructure/src/main/java/com/sunandan/permutation/RepeatedString.java
@@ -22,7 +22,7 @@ public class RepeatedString {
     }
 
     public boolean repeatedSubstringPatternUsingRecursion(String input) {
-        if(input.length() % 2 != 0) return false;
+        if (input.length() % 2 != 0) return false;
         return repeatedSubstringPatternUsingRecursion(input, input.length() / 2);
     }
 
@@ -30,14 +30,14 @@ public class RepeatedString {
         if (pivot < 1)
             return false;
         String subString = actualString.substring(0, pivot);
-        if(repeat(subString, actualString.length()/pivot).equals(actualString))
+        if (repeat(subString, actualString.length() / pivot).equals(actualString))
             return true;
-        return repeatedSubstringPatternUsingRecursion(actualString, pivot-1);
+        return repeatedSubstringPatternUsingRecursion(actualString, pivot - 1);
     }
 
-    private String repeat(String input, int numberOfTimes){
+    private String repeat(String input, int numberOfTimes) {
         StringBuilder output = new StringBuilder();
-        for(int i = 0 ; i<numberOfTimes;i++)
+        for (int i = 0; i < numberOfTimes; i++)
             output.append(input);
         return output.toString();
     }
