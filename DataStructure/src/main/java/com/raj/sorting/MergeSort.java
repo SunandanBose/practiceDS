bfdbcb11f3dd1ca2971916166f909cbbfae53960
Sunandan Bose <sunandan.riders@gmail.com>
2020-09-27 11:16:08 +0530

    Modified Merge Sort

--- DataStructure/src/main/java/com/raj/sorting/MergeSort.java
+++ DataStructure/src/main/java/com/raj/sorting/MergeSort.java
@@ -1,10 +1,12 @@
- package com.raj.sorting;
+package com.raj.sorting;
+
 import java.util.*;
-class MergeSort{
+
+class MergeSort {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        List<Integer> x = new ArrayList(Arrays.asList(2,1,4,3,-2,56));        
+        List<Integer> x = new ArrayList(Arrays.asList(2, 1, 4, 3, -2, 56));
         mergeSort(x);
         System.out.println(x);
     }
@@ -12,11 +14,11 @@ class MergeSort{
 
     // 1,2 13,16
 
-    private static void mergeSort(List<Integer> list){
+    private static void mergeSort(List<Integer> list) {
         // recurse until single element is left -> done
         // Get left and right from list -> done
         // Recurse -> done
-        if (list.size() <= 1){
+        if (list.size() <= 1) {
             return;
         }
         int mid = list.size() / 2;
@@ -28,33 +30,65 @@ class MergeSort{
         int leftIndex = 0;
         int rightIndex = 0;
         int resultListIndex = 0;
-        
+
         // merge left and right
 
-        while(leftIndex < left.size() || rightIndex < right.size()){
+        while (leftIndex < left.size() || rightIndex < right.size()) {
             System.out.println(left);
             System.out.println(right);
-            if (leftIndex == left.size() || rightIndex == right.size()){
+            if (leftIndex == left.size() || rightIndex == right.size()) {
                 break;
             }
-            if (left.get(leftIndex) < right.get(rightIndex)){
+            if (left.get(leftIndex) < right.get(rightIndex)) {
                 list.set(resultListIndex++, left.get(leftIndex++));
                 continue;
             }
-            if (left.get(leftIndex) > right.get(rightIndex)){
+            if (left.get(leftIndex) > right.get(rightIndex)) {
                 list.set(resultListIndex++, right.get(rightIndex++));
                 continue;
             }
         }
-        if(leftIndex != left.size()){
+        if (leftIndex != left.size()) {
             for (int i = leftIndex; i < left.size(); i++) {
                 list.set(resultListIndex++, left.get(leftIndex++));
             }
         }
-        if(rightIndex != right.size()){
+        if (rightIndex != right.size()) {
             for (int i = rightIndex; i < right.size(); i++) {
                 list.set(resultListIndex++, right.get(rightIndex++));
             }
         }
     }
+
+
+    //mergeSort SimplerVersion
+    public List<Integer> sort(List<Integer> list) {
+        if (list == null)
+            return list;
+        if (list.size() <= 1)
+            return list;
+        int middle = list.size() / 2;
+        List<Integer> left = list.subList(0, middle);
+        List<Integer> right = list.subList(middle, list.size());
+        return merge(sort(left), sort(right));
+    }
+
+    private List<Integer> merge(List<Integer> left, List<Integer> right) {
+        List<Integer> result = new ArrayList<>();
+        int leftIndex = 0;
+        int rightIndex = 0;
+        while ((leftIndex < left.size() && (rightIndex < right.size()))) {
+            if (left.get(leftIndex) < right.get(rightIndex)) {
+                result.add(left.get(leftIndex));
+                leftIndex++;
+            } else {
+                result.add(right.get(rightIndex));
+                rightIndex++;
+            }
+        }
+        result.addAll(left.subList(leftIndex, left.size()));
+        result.addAll(right.subList(rightIndex, right.size()));
+        return result;
+    }
+
 }
