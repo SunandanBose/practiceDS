f1c3298c44ce9f170bcdcecbf719d7ebdfdb9e11
Sunandan Bose <sunandan.riders@gmail.com>
2020-10-18 22:58:56 +0530

    update readme script

--- updateReadMe.sh
+++ updateReadMe.sh
@@ -1,7 +1,7 @@
 lastLine=$(tail -n 1 README.md)
-echo $lastLine
+#echo $lastLine
 serialNumber=$(echo "$lastLine" | cut -d "|" -f1)
-echo $((serialNumber+1))
+#echo $((serialNumber+1))
 newSerialNumber=$((serialNumber+1))
 newlyAddedFileNames=$(git diff-tree --diff-filter=A --no-commit-id --name-status -r "$local_sha" | tr 'A ' ' ')
 echo $commit 
@@ -10,13 +10,21 @@ read -ra fileArray <<<"$newlyAddedFileNames" #reading commit str as an array as 
   
 for i in "${fileArray[@]}";
 do  
+	if [[ $i =~ "Test.java" ]]; then
+		continue
+	fi
 	echo Do you want to continue\?\(y\/n\)
 	read ans
 	if [[ "$ans" == "Y" || "$ans" == "y" ]]; then
     	echo Enter the code link for file $i
 		read codeLink
 		echo codeLink you have entered is $codeLink
-		echo $newSerialNumber \| Distribute Candies to People \|[Explanation]\($codeLink\)\| [Code]\($i\)\|| >> README.md
+		echo Enter problem name
+		read problemName
+		testFileName=$(echo "$i" | sed -e 's/\/main\//\/test\//g' | sed -e 's/\.java/Test\.java/g')
+		echo "Line added in README.md"
+		echo $newSerialNumber \| $problemName \|[Explanation]\($codeLink\)\| [Code]\($i\)\|[Test]\($testFileName\)\|
+		echo $newSerialNumber \| $problemName \|[Explanation]\($codeLink\)\| [Code]\($i\)\|[Test]\($testFileName\)\|| >> README.md
 	fi
 	
 	  
