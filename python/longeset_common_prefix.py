76277caf9e31f4f22148ed48e6bba497553e1c27
Raj Kumar <raj.kumar@glovoapp.com>
2023-07-17 08:25:11 +0200

    Longest common prefix basic version

---
+++ python/longeset_common_prefix.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/longest-common-prefix/
+
+def longestCommonPrefix(strings: list[str]) -> str:
+	res = ''
+	current_char = strings[0][0]
+	current_char_index = 0
+	stop = False
+	while stop != True:
+		for string in strings:
+			if current_char != string[current_char_index]:
+				stop = True
+				break
+		print (res, current_char, current_char_index)
+		current_char_index = current_char_index + 1
+		if stop != True:
+			res = res + current_char
+
+		if current_char_index < len(strings[0]):
+			current_char = strings[0][current_char_index] 
+
+	return res
+
+print(longestCommonPrefix(['abc', 'abd', 'abe']))
