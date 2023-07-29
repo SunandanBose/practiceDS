06ef7f1c9f69ab1a8aef554895860cfb36dcf0ba
Raj Kumar <raj.kumar@glovoapp.com>
2023-07-29 11:06:06 +0200

    Add link to 2 sum and fix the code

--- python/2_sum.py
+++ python/2_sum.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/two-sum/
 
 nums = [2,7,11,15]
 target = 9
@@ -5,8 +6,6 @@ target = 9
 
 def two_sum(numbers: list[int], target: int) -> list[int]:
 	for i in range(0, len(numbers)):
-		if numbers[i] >= target:
-			continue
 		for j in range(i+1, len(numbers)):
 			if numbers[i] + numbers[j] == target:
 				return[numbers[i],numbers[j]]
