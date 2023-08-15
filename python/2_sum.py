a0b2051667702a43a628f0f1e08a3780c086cffb
Raj Kumar <raj.kumar@glovoapp.com>
2023-08-15 16:57:36 +0200

    Another solution to 2 sum

--- python/2_sum.py
+++ python/2_sum.py
@@ -4,7 +4,8 @@ nums = [2,7,11,15]
 target = 9
 
 
-def two_sum(numbers: list[int], target: int) -> list[int]:
+# O[n2]
+def two_sum_1(numbers: list[int], target: int) -> list[int]:
 	for i in range(0, len(numbers)):
 		for j in range(i+1, len(numbers)):
 			if numbers[i] + numbers[j] == target:
@@ -12,4 +13,18 @@ def two_sum(numbers: list[int], target: int) -> list[int]:
 
 	return []
 
-print(two_sum(nums, target))
+# O[1]
+# We are saving all numbers in a map
+# Checking if current number minus the target is already in the map
+# if yes, return it.
+def two_sum_2(numbers: list[int], target: int) -> list[int]:
+	tracker_dict = {}
+	for i in range(0, len(numbers)):
+		if (target - numbers[i]) in tracker_dict:
+			return [i, tracker_dict[target - numbers[i]]]
+		tracker_dict[numbers[i]] = i
+
+	return []
+
+print(two_sum_1(nums, target))
+
