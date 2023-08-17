cd63dd659acf679cd0b65373d0853bb34a23a838
Raj Kumar <raj.kumar@glovoapp.com>
2023-08-17 08:52:27 +0200

    Adding 2 numbers

--- python/add_two_numbers.py
+++ python/add_two_numbers.py
@@ -1,47 +1,31 @@
-# https://leetcode.com/problems/add-two-numbers/
+# https://leetcode.com/problems/add-two-numbers/description/
 
-from typing import Optional
+# Adding 2 numbers represented as lists
+
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        num1 = ''
-        num2 = ''
-        while (l1 is not None):
-            num1 = num1 + str(l1.val)
-            l1 = l1.next
-        while (l2 is not None):
-            num2 = num2 + str(l2.val)
-            l2 = l2.next
+        head = ListNode(0, None)
+        result = head
+        extra = 0
 
-        num1 = num1[::-1]
-        num2 = num2[::-1]
-        sum = int(num1) + int(num2)
-        head = ListNode(int(sum%10), None)
-        sum = int(sum / 10)
-        res = head
-        while (sum != 0):
-            res.next = ListNode(int(sum%10), None)
-            res = res.next
-            sum = int(sum / 10)
-
-        return head
-
-
-l1 = ListNode(2, None)
-l1.next = ListNode(4, None)
-l1.next.next = ListNode(3, None)
-
-l2 = ListNode(5, None)
-l2.next = ListNode(6, None)
-l2.next.next = ListNode(4, None)
-
-
-sol = Solution().addTwoNumbers(l1, l2)
-while (sol is not None):
-    print(sol.val)
-    sol = sol.next
+        while extra or l1 or l2:
+            stack1_pop = l1.val if l1 else 0
+            stack2_pop = l2.val if l2 else 0
+            sum = extra + stack1_pop + stack2_pop
+            extra = 0
+            if sum > 9:
+                extra = 1
+                sum = sum % 10
+            
+            result.next = ListNode(sum, None)
+            l1 = l1.next if l1 else l1
+            l2 = l2.next if l2 else l2
+            result = result.next
+            
+        return head.next
