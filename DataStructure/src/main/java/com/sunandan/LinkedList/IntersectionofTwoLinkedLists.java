62e5a166e257d9f6b8c8e266cb274d0c4b7a5262
Raj <rajkrjsr17@gmail.com>
2024-02-15 11:55:00 +0100

    Update IntersectionofTwoLinkedLists.java
    Add description

--- DataStructure/src/main/java/com/sunandan/LinkedList/IntersectionofTwoLinkedLists.java
+++ DataStructure/src/main/java/com/sunandan/LinkedList/IntersectionofTwoLinkedLists.java
@@ -1,7 +1,10 @@
 package com.sunandan.LinkedList;
 
 public class IntersectionofTwoLinkedLists {
-
+    // raj: Another solution is to put all elemnts in a new map while iterating list1
+    // Iterate over list2 and check the map
+    // Return if match or else exit
+    
     public Node getIntersectionNode(Node headA, Node headB) {
         Node head1 = headA, head2 = headB;
         while (head1 != head2) {
