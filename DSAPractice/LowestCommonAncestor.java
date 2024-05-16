9a00fbd116c211158b7ddfbd46bb813edb822fcb
Sunandan Bose <sunandan.riders@gmail.com>
2024-05-16 22:24:56 +0530

    Create LowestCommonAncestor.java

---
+++ DSAPractice/LowestCommonAncestor.java
@@ -0,0 +1,21 @@
+class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return null;
+        if(p.val<root.val && q.val<root.val) {
+            return lowestCommonAncestor(root.left,p,q);
+        }
+        else if(p.val>root.val && q.val>root.val){
+            return lowestCommonAncestor(root.right,p,q);
+        }
+        else
+            return root;
+    }
+
+}
+
+public class TreeNode {
+   int val;
+   TreeNode left;
+   TreeNode right;
+   TreeNode(int x) { val = x; }
+}
